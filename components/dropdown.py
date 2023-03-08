3fb8fd9e868bb96be5c8ca79a87d2e31608a8b74
Junyu Wu <123514576+Lazyu1211@users.noreply.github.com>
2023-03-08 11:33:29 -0500

    Update dropdown.py

--- components/dropdown.py
+++ components/dropdown.py
@@ -4,7 +4,7 @@ from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
-from utill import get_opentime
+from utill import get_opentime, get_closetime
 
 def render(app):
     list_open = get_opentime()
@@ -22,14 +22,41 @@ def render(app):
             dcc.Dropdown(
                 options=all_open,
                 multi=True,
-                id = "open_dropdown",
-                value= 1
+                id = "open_dropdown"
             ),
             dbc.Button(
                 children=["选择所有选项"],
                 color="primary",
                 className="me-1",
                 id="select_all_button",
+                n_clicks=0
+            )
+        ]
+    )
+
+def render1(app):
+    list_close = get_closetime()
+    all_close = [{'label':i,'value':i} for i in list_close]
+    @app.callback(
+    Output(component_id='close_dropdown', component_property='value'),
+    Input(component_id='select_all_button1', component_property='n_clicks')
+    )
+    def update_all_open(n):
+        return list_close
+
+    return html.Div(
+        children=[
+            html.H6("关闭营业时间选择"),
+            dcc.Dropdown(
+                options=all_close,
+                multi=True,
+                id = "close_dropdown"
+            ),
+            dbc.Button(
+                children=["选择所有选项"],
+                color="primary",
+                className="me-1",
+                id="select_all_button1",
                 n_clicks=0
             )
         ]
